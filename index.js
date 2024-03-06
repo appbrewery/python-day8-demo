0f33d9df6efe1878be2e8bb3443ac3b40665ee27
Angela Yu <8798027+angelabauer@users.noreply.github.com>
2024-03-06 11:13:17 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,203 @@
+//Current line
+var CurrentId = undefined;
+
+var inputValues = [];
+const inputPrompts = [
+  "Type your message:",
+  "Type the shift number:",
+  "Type 'yes' if you want to go again. Otherwise type 'no'.",
+];
+
+logo = `        
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     'Y8 a8P_____88 I8[    "" ""     'Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  '"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ '"Ybbd8"' '"8bbdP"Y8  '"Ybbd8"' '"YbbdP"' '"8bbdP"Y8 88   
+            88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ '"Ybbd8"' 88 88'YbbdP"'  88       88  '"Ybbd8"' 88          
+              88                                             
+              88           
+`;
+
+const alphabet = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
+//Click Run
+$(document).ready(function () {
+  $("#run-button").click(function () {
+    $("#Content").empty();
+    restart();
+  });
+});
+
+function restart() {
+  inputValues = [];
+  NewLine(logo, false);
+  NewLine("Type 'encode' to encrypt, type 'decode' to decrypt:", true);
+}
+
+//Enter button
+$(document).on("keydown", function (e) {
+  var x = event.which || event.keyCode;
+  if (x === 13 || x == 13) {
+    var consoleLine = $("#" + CurrentId + " input").val();
+    inputValues.push({ id: CurrentId, val: consoleLine.toLowerCase() });
+
+    if (inputValues.length == 3) {
+      NewLine(
+        caesar(
+          inputValues[0].val,
+          inputValues[1].val,
+          Number(inputValues[2].val)
+        ),
+        false
+      );
+      NewLine(inputPrompts[2], true);
+    }
+
+    if (inputValues.length > inputPrompts.length) {
+      if (inputValues[3].val == "yes") {
+        restart();
+        return;
+      } else {
+        NewLine("Goodbye", false);
+        return;
+      }
+    }
+
+    $(".console-carrot").remove();
+    if (inputValues.length < 3) {
+      NewLine(inputPrompts[inputValues.length - 1], true);
+    }
+  }
+});
+$(document).on("keydown", function (e) {
+  var x = event.which || event.keyCode;
+  var line = $("#" + CurrentId + " input");
+  var length = line.val().length;
+  if (x != 8) {
+    line.attr("size", 1 + length);
+  } else {
+    line.attr("size", length * 0.95);
+  }
+  if (length === 0) {
+    $("#" + CurrentId + " input").attr("size", "1");
+  }
+});
+$(document).on("click", function (e) {
+  $("#" + CurrentId + " input").focus();
+});
+
+//New line
+function NewLine(text, isPrompt) {
+  $(".console-carrot").remove();
+  if (CurrentId !== undefined) {
+    $("#" + CurrentId + " input").prop("disabled", true);
+  }
+  CurrentId = "consoleInput-" + GenerateId();
+
+  if (isPrompt) {
+    $("#Content").append("<div>" + text + "</div>");
+    $("#Content").append(
+      '<div id="' +
+        CurrentId +
+        '">' +
+        '<input autocomplete="off" autocorrect="off" autocapitalize="off" spellcheck="false" type="text" class="terminal-input" /><div class="console-carrot"></div></div>'
+    );
+    $("#" + CurrentId + " input").focus();
+    $("#" + CurrentId + " input").attr("size", "1");
+  } else {
+    $("#Content").append('<div id="' + CurrentId + '">' + text + "</div>");
+  }
+  document.getElementById(CurrentId).scrollIntoView();
+}
+
+function caesar(cipher_direction, start_text, shift_amount) {
+  let end_text = "";
+  if (cipher_direction == "decode") {
+    shift_amount *= -1;
+  }
+  for (char in start_text) {
+    if (alphabet.includes(start_text[char])) {
+      const position = alphabet.indexOf(start_text[char]);
+      const new_position = position + shift_amount;
+      end_text += alphabet[new_position];
+    } else {
+      end_text += start_text[char];
+    }
+  }
+  console.log(end_text);
+
+  return `Here's the ${cipher_direction}d result: ${end_text}`;
+}
+
+function GenerateId() {
+  return Math.random().toString(16).slice(2);
+}
+
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
